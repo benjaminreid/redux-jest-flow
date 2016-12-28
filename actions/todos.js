18adb0852a3e926fbfb38669e5dda7041b11867b
Benjamin Reid <benjamin@nouveller.com>
2016-12-28 16:36:16 +0000

    Make the type of TodoAction required and use this in the action creators

--- actions/todos.js
+++ actions/todos.js
@@ -1,12 +1,14 @@
 // @flow
+import type { TodoAction } from './../types';
+
 export const TODO_ADD = 'TODO_ADD';
 export const TODO_CLEAR = 'TODO_CLEAR';
 
-export const addTodo = (text: string):Object => ({
+export const addTodo = (text: string):TodoAction => ({
   type: TODO_ADD,
   text,
 });
 
-export const clearTodos = ():Object => ({
+export const clearTodos = ():TodoAction => ({
   type: TODO_CLEAR,
 });
--- types/index.js
+++ types/index.js
@@ -1,5 +1,5 @@
 // @flow
 export type TodoAction = {
-  type?: string,
+  type: string,
   text?: string,
 };
